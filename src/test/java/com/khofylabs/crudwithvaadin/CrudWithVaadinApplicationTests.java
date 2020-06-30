0773eb94c6a5288eecddd6cb98c2c20ea706dcad
khoffy <khoffy.tipoh@gmail.com>
2020-06-30 22:41:10 +0000

    first commit for the code

---
+++ src/test/java/com/khofylabs/crudwithvaadin/CrudWithVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khofylabs.crudwithvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWithVaadinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
